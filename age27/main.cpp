23d6daa92b99be65e12b73df9dd5a61e2f3dbfca
DaniilLeonov <baslook23@gmail.com>
2021-12-21 20:20:54 +0300

    ineff done

--- age27/main.cpp
+++ age27/main.cpp
@@ -16,9 +16,15 @@ void div_()
     vector<int> numbers(n, 0);
     for (int i(0); i < n; i++)
         cin >> numbers[i];
+    int pairs = 0;
     for (int j = 0; j < n; ++j)
         for (int i = j + 1; i < n; ++i)
-            cout << numbers[j] * numbers[i] << endl;
+            {
+                int cur_prod = numbers[j] * numbers[i];
+                if (cur_prod % 55 == 0)
+                    pairs++;
+            }
+    cout << pairs;
 }
 int main()
 {
