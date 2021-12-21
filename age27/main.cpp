f2ce2869079c486246afb88f65fbfe18b31195e3
DaniilLeonov <baslook23@gmail.com>
2021-12-21 22:53:54 +0300

    reads and shows numbers with first and second condition,

--- age27/main.cpp
+++ age27/main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void num_sum_max_distance_ineff()
 {
     /**
-     1 считать последовательность чисел
-     2 вывести пары удовлетворяющие первому условию
-     3 вывести пары удовлетворяющие второму условию
-     4 вывести ответ
+     1)считать последовательность чисел
+     2) вывести пары удовлетворяющие первому условию
+     3) вывести пары удовлетворяющие второму условию
+     4) вывести ответ
     */
+   int n;
+   cin >> n;
+   vector<int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
+
+   for (int i = 0; i < n; ++i)
+    for (int j = i + 1; j < n; ++j)
+    {
+        if ((abs(i - j) < 10) and ((numbers[i] + numbers[j]) % 31 == 0))
+            cout << numbers[i] << ' ' << numbers[j] << endl;
+    }
 }
 int main()
 {
