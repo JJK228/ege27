924fe23d9bef1b23118ecff9433c1eda0e51582e
DaniilLeonov <baslook23@gmail.com>
2021-12-21 23:02:39 +0300

    done with worm, i want sleep

--- age27/main.cpp
+++ age27/main.cpp
@@ -14,8 +14,27 @@ void div()
 
     **/
 
+    int n, count = 0;
+    cin >> n;
+    vector<int> counters(33, 0);
 
+    vector<int> worm_num(6, 0);
+
+    for (int i(0); i < 6; i++)
+        cin >> worm_num[i];
+
+    for (int i = 6; i < n; ++i)
+    {
+        counters[worm_num[i % 6] % 33] += 1;
+
+        cin >> worm_num[i % 6];
+
+        count += counters[(33 - (worm_num[i % 6] % 33)) % 33];
+
+    }
+        cout << count;
 }
+
 int main()
 {
     div();
