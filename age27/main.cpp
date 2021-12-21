952c81478739553ca00b1b475af8a4c8b28b6de6
DaniilLeonov <baslook23@gmail.com>
2021-12-21 22:44:41 +0300

    fixed again with shows numbers, with second condition task done

--- age27/main.cpp
+++ age27/main.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void  div()
+void num_sum_max_distance_ineff()
 {
     /**
+     1)считать последовательность чисел
+     2) вывести пары удовлетворяющие первому условию
+     3) вывести пары удовлетворяющие второму условию
+     4) вывести ответ
+    */
+   int n;
+   cin >> n;
+   vector<int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
 
-
-
-    1 считать числа
-    2 вывести пары
-    3 вывести пары с минимальным расстоянием
-    4 вместо вывода пары подсчитать сумму
-    5 найти минимум среди сумм
-    **/
+    int count = 0;
+   for (int i = 0; i < n; ++i)
+    for (int j = i + 1; j < n; ++j)
+    {
+        if ((abs(i - j) < 10) and ((numbers[i] + numbers[j]) % 31 == 0))
+            count += 1;
+    }
+    cout << count;
 }
-
 int main()
 {
-    div();
+    num_sum_max_distance_ineff();
     return 0;
 }
