87f97f4d8d46dfabe110b23ffb5816129b7e1927
DaniilLeonov <baslook23@gmail.com>
2021-12-21 23:05:13 +0300

    added plan with show numbers

--- age27/main.cpp
+++ age27/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
+void num_sum_multiple_distance_ineff()
+{
+    /**
+     1 считать последовательность чисел
+     2 вывести пары удовлетворяющие первому условию
+     3 вывести пары удовлетворяющие второму условию
+     4 вывести ответ
+    */
+   int n;
+   cin >> n;
+   vector<int> numbers(n, 0);
+    for (int i(0); i < n; i++)
+        cin >> numbers[i];
+
+   for (int i = 0; i < n; ++i)
+    for (int j = i + 1; j < n; ++j)
+    {
+        if (abs(i - j) % 7 == 0)
+            cout << numbers[i] << ' ' << numbers[j] << endl;
+    }
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    num_sum_multiple_distance_ineff();
     return 0;
 }
