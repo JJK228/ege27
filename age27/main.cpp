13cea5630b7092ab01cf56bcf5dcc1a5c375ec18
DaniilLeonov <baslook23@gmail.com>
2021-12-21 21:31:22 +0300

    read and show numbers

--- age27/main.cpp
+++ age27/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 void div_()
 {
     /**
@@ -8,10 +9,19 @@ void div_()
      2 найти наибольшее четное и наибольшее нечетное числа
      3 вывести сумму наибольших чисел
     */
+    int n(0);
+    cin >> n;
+    vector <int> numbers(n);
+    for (int i(0); i<n; i++)
+            cin>> numbers[i];
+    cout << endl;
+    for (int i(0); i<n; i++)
+        cout << numbers[i] << " ";
+
 }
 
 int main()
 {
-    div_;
+    div_();
     return 0;
 }
