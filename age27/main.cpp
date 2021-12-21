5d5720f2189cba839f50798210a7b658b2443def
DaniilLeonov <baslook23@gmail.com>
2021-12-21 21:29:40 +0300

    added plan

--- age27/main.cpp
+++ age27/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+void div_()
+{
+    /**
+     1 считать и запомнить числа
+     2 найти наибольшее четное и наибольшее нечетное числа
+     3 вывести сумму наибольших чисел
+    */
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    div_;
     return 0;
 }
