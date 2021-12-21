c333e854eda53288fa45dcfaf7cfff89b4e183f5
DaniilLeonov <baslook23@gmail.com>
2021-12-21 21:27:36 +0300

    done

--- age27/main.cpp
+++ age27/main.cpp
@@ -15,11 +15,15 @@ void div_()
     cin >> n;
     vector <int> numbers(n);
     for (int i(0); i<n; i++)
-            cin>> numbers[i];
-    cout << endl;
+            cin >> numbers[i];
+    int max_sum(0);
     for (int j(0); j<n-1; j++)
         for (int i(j+1); i<n; i++)
-            cout << numbers[j]<<" "<<numbers[i]<< endl;
+        {int cur_sum(numbers[j] + numbers[i]);
+            if (cur_sum % 2 != 0 and cur_sum>max_sum)
+                max_sum = cur_sum;
+        }
+    cout <<max_sum;
 }
 int main()
 {
