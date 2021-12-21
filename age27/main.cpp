811100a9c481c4275a8363510015fdafce10a0b3
DaniilLeonov <baslook23@gmail.com>
2021-12-21 21:37:19 +0300

    done

--- age27/main.cpp
+++ age27/main.cpp
@@ -9,7 +9,7 @@ void div_()
      2 найти наибольшее четное и наибольшее нечетное числа
      3 вывести сумму наибольших чисел
     */
-    int n(0);
+     int n(0);
     cin >> n;
     vector <int> numbers(n);
     int max_div_2 = -1001;
@@ -19,14 +19,17 @@ void div_()
             cin>> numbers[i];
             if (numbers[i] % 2 == 0)
                 {   if (numbers[i] > max_div_2)
-                        max_div_2 = numbers[i];
+                    max_div_2 = numbers[i];
                 }
 
             else if (numbers[i] % 2 != 0)
                     if (numbers[i] > max_not_div_2)
                         max_not_div_2 = numbers[i];
             }
-    cout << max_not_div_2 << ' ' << max_div_2;
+
+    if (max_not_div_2 + max_div_2 < 0)
+        cout << -1;
+    else cout << max_not_div_2 + max_div_2;
 
 }
 
