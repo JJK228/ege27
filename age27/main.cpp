d72b45c53746990e274704c063e40aaa6c35ccf8
DaniilLeonov <baslook23@gmail.com>
2021-12-21 22:31:34 +0300

    reads and shows numbers

--- age27/main.cpp
+++ age27/main.cpp
@@ -12,6 +12,16 @@ void if_()
     3а. все классно
     4.   найти мин
     **/
+    int n(0);
+    cin >> n;
+    int min_num(1000);
+    int min_sum(2000);
+    vector<int> numbers(4);
+    for (int i(0); i < 4; i++)
+        cin >> numbers[i];
+    for (int j(0); j < n-4; j++)
+
+    cout << min_sum << endl;
 }
 
 int main()
