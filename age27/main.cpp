b81621fa9e27148f154429072ffc7c7a2a15c280
DaniilLeonov <baslook23@gmail.com>
2021-12-21 21:21:31 +0300

    added plan, reads and shows numbers

--- age27/main.cpp
+++ age27/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
+void div_()
+{
+    /**
+    1 считать и сохранить все числа
+    2 вывести все пары
+    3 вместо пары вывести суммы
+    4 проверить делимость суммы
+    5 вывести только наибольшую сумму
+    */
+    int n(0);
+    cin >> n;
+    vector <int> numbers(n);
+    for (int i(0); i<n; i++)
+            cin>> numbers[i];
+    cout << endl;
+    for (int i(0); i<n; i++)
+        cout << numbers[i] << " ";
+
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    div_();
     return 0;
 }
