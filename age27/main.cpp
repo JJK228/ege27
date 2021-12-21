e38dd0641dcca22cddaa75da61956a5c66932990
DaniilLeonov <baslook23@gmail.com>
2021-12-21 20:58:02 +0300

    added counter

--- age27/main.cpp
+++ age27/main.cpp
@@ -13,11 +13,20 @@ void div_()
     int n6 = 0, n3 = 0, n2 = 0, no = 0;
     int n = 0, cur = 0;
     cin >> n;
-    for (int i (0); i<n; i++)
-        {
-            cin >> cur;
-            cout << cur << ' ';
-        }
+    for (int i = 0; i<n; i++)
+    {
+        cin >> cur;
+        if (cur % 6 == 0)
+            n6++;
+        else if (cur % 2 ==0)
+            n2++;
+        else if (cur % 3 == 0)
+            n3++;
+    }
+
+    no = n - n6 - n3 - n2;
+    cout << n6 << ' ' << n3 << ' ' << n2 << ' ' <<  no;
+
 
 }
 
