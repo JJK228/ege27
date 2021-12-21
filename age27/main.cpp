6c03266e893eaf31aed40c7ef98dda052cdf69c2
DaniilLeonov <baslook23@gmail.com>
2021-12-21 22:54:39 +0300

    task done

--- age27/main.cpp
+++ age27/main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 void num_sum_max_distance_ineff()
 {
     /**
-     1)считать последовательность чисел
-     2) вывести пары удовлетворяющие первому условию
-     3) вывести пары удовлетворяющие второму условию
-     4) вывести ответ
+     1 считать последовательность чисел
+     2 вывести пары удовлетворяющие первому условию
+     3 вывести пары удовлетворяющие второму условию
+     4 вывести ответ
     */
    int n;
    cin >> n;
@@ -17,12 +17,14 @@ void num_sum_max_distance_ineff()
     for (int i(0); i < n; i++)
         cin >> numbers[i];
 
+    int count = 0;
    for (int i = 0; i < n; ++i)
     for (int j = i + 1; j < n; ++j)
     {
         if ((abs(i - j) < 10) and ((numbers[i] + numbers[j]) % 31 == 0))
-            cout << numbers[i] << ' ' << numbers[j] << endl;
+            count += 1;
     }
+    cout << count;
 }
 int main()
 {
